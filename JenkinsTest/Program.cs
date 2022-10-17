78c1368b644b9ca1a77aba3db2db105dcabc5430
Christian Poschmann <christian.poschmann@scheugenpflug-dispensing.com>
2022-10-17 13:19:04 +0200

    enter text

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Test");
+
+
+while(true)
+{
+    string? s = "Enter text: " + Console.ReadLine();
+    Console.WriteLine(s + "!!!");
+}
