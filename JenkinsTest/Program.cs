56cdf4c97e809634cac4596b5c6f325e18eb6173
Christian Poschmann <christian.poschmann@scheugenpflug-dispensing.com>
2022-10-17 14:11:32 +0200

    exit

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -7,5 +7,8 @@ while(true)
 {
     Console.Write("Enter text: ");
     string? s = Console.ReadLine();
+
+    if (s.ToLower() == "exit")
+        break;
     Console.WriteLine("Your text: " + s);
 }
